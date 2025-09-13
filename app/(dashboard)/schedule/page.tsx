63466b33d98c69a24f010e9950e77355d9465fe7
Wildan Maulana <wildanmh515@gmail.com>
2025-09-13 21:27:22 +0700

    tesa

--- app/(dashboard)/schedule/page.tsx
+++ app/(dashboard)/schedule/page.tsx
@@ -1,33 +1,38 @@
 // app/(dashboard)/schedule/page.tsx
-import { DayOfWeek } from '@/app/types';
-import { getClassSchedules } from './actions';
-import { getCourses } from '../courses/actions';
-import ScheduleView from './ScheduleView';
-import { ScheduleWithCourse } from '@/app/components/ScheduleCard';
+import { DayOfWeek } from "@/app/types";
+import { getClassSchedules } from "./actions";
+import { getCourses } from "../courses/actions";
+import ScheduleView from "./ScheduleView";
+import { ScheduleWithCourse } from "@/app/components/ScheduleCard";
 
 export default async function SchedulePage() {
   const schedules = await getClassSchedules();
   const courses = await getCourses();
 
-  const groupedSchedules = schedules.reduce((acc, schedule) => {
-    const day = schedule.dayOfWeek;
-    if (!acc[day]) {
-      acc[day] = [];
-    }
-    acc[day].push(schedule);
-    return acc;
-  }, {} as Record<DayOfWeek, ScheduleWithCourse[]>);
+  const groupedSchedules = schedules.reduce(
+    (acc: Record<DayOfWeek, ScheduleWithCourse[]>, schedule) => {
+      const day = schedule.dayOfWeek;
+      if (!acc[day]) {
+        acc[day] = [];
+      }
+      acc[day].push(schedule);
+      return acc;
+    },
+    {} as Record<DayOfWeek, ScheduleWithCourse[]>
+  );
 
   for (const day in groupedSchedules) {
-    groupedSchedules[day].sort((a, b) => 
-      new Date(a.startTime).getTime() - new Date(b.startTime).getTime()
+    groupedSchedules[day].sort(
+      (a, b) =>
+        new Date(a.startTime).getTime() - new Date(b.startTime).getTime()
     );
   }
 
-
-  return <ScheduleView 
-           initialSchedules={schedules} 
-           initialCourses={courses} 
-           initialGroupedSchedules={groupedSchedules} 
-         />;
+  return (
+    <ScheduleView
+      initialSchedules={schedules}
+      initialCourses={courses}
+      initialGroupedSchedules={groupedSchedules}
+    />
+  );
 }
